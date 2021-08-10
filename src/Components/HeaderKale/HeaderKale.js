6cf84f155c915ed79c45e2671114297354c0d868
Alejodg9 <digiovine.a@gmail.com>
2021-08-09 23:21:12 -0300

    arreglo nombre

--- src/Components/HeaderKale/HeaderKale.js
+++ src/Components/HeaderKale/HeaderKale.js
@@ -12,7 +12,7 @@ const Header = () => {
                     <div className="heroImg"></div>
                 </div>
                 <div className="der" >
-                    <h1>Hola! <br /> Somos <span className="green" >Kale</span>X</h1>
+                    <h1>Hola! <br /> Somos <span className="green" >Kale</span>indeX</h1>
                     <p>Buscamos combinar <span className="green" > innovación</span>, <span className="green" >lógica</span> y <span className="green" >diseño</span> para crear productos digitales de alto valor agregado. Siempre abiertos a nuevos desafios!</p>
                     <button class="button button1">Contanos tu idea</button>
                 </div>
